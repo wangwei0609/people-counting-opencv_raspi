502ed7817f59a09484d37d912449a3595ad8f73b
Wang Wei <wangwei0609@gmail.com>
2018-12-20 11:26:54 +0800

    first commit

---
+++ foo_ncs.sh
@@ -0,0 +1,1 @@
+python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 -g mobilenetgraph_frdc25403
