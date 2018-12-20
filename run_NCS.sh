dd0ca082c77366afe84b8c08d2ee1e9adc2d0b64
Wang Wei <wangwei0609@gmail.com>
2018-12-20 16:27:55 +0800

    update

---
+++ run_NCS.sh
@@ -0,0 +1,5 @@
+python people_counter.py \
+--prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
+--input videos/example_01.mp4 \
+-g mobilenetgraph_frdc25403
